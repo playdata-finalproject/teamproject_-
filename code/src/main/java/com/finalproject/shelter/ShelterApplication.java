2dacc498d008dda0f8a202da07fafb27b7d3e3c1
jihyeonmun <jhmoon1994@gmail.com>
2021-03-09 00:39:50 +0900

    commit check(210309)

--- code/src/main/java/com/finalproject/shelter/ShelterApplication.java
+++ code/src/main/java/com/finalproject/shelter/ShelterApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShelterApplication {
-    ////
+    //////
     //////
     public static void main(String[] args) {
         SpringApplication.run(ShelterApplication.class, args);
