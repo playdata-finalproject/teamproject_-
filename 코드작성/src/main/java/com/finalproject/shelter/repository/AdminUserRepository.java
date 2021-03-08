95ba040f7a139bdc71d3cde84f109dc5161f8d9a
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-03-08 23:52:18 +0900

    type: example

--- 코드작성/src/main/java/com/finalproject/shelter/repository/AdminUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.finalproject.shelter.repository;
-
-import com.finalproject.shelter.model.entity.AdminUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminUserRepository extends JpaRepository<AdminUser, Long> {
-}
--- 코드작성/src/test/java/com/finalproject/shelter/service/BoardLogicServiceTest.java
+++
@@ -1,8 +0,0 @@
-package com.finalproject.shelter.service;
-
-import com.finalproject.shelter.ShelterApplicationTests;
-import org.junit.jupiter.api.Test;
-
-public class BoardLogicServiceTest extends ShelterApplicationTests {
-
-}
