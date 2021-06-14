4c3af79013e5eb7d2b94480b74a90ee3e0544242
jeeseungbae <https://github.com/jeeseungbae/TIMD_project.git>
2021-06-14 18:24:36 +0900

    refector : BoardLogicService,AnswerLogicService클래스 생성자로 의존성을 주입받는 경우로 변경

--- app/src/main/java/com/finalproject/shelter/presentation/controller/view/board/BoardViewPageController.java
+++ app/src/main/java/com/finalproject/shelter/presentation/controller/view/board/BoardViewPageController.java
@@ -4,22 +4,24 @@ import com.finalproject.shelter.domain.model.entity.noticationDomain.Answer;
 import com.finalproject.shelter.domain.model.entity.noticationDomain.Board;
 import com.finalproject.shelter.business.service.logic.AnswerLogicService;
 import com.finalproject.shelter.business.service.logic.BoardLogicService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequestMapping("/board/view")
 public class BoardViewPageController {
-    @Autowired
-    private BoardLogicService boardLogicService;
-    @Autowired
-    private AnswerLogicService answerLogicService;
+
+    private final BoardLogicService boardLogicService;
+    private final AnswerLogicService answerLogicService;
+
+    public BoardViewPageController(BoardLogicService boardLogicService, AnswerLogicService answerLogicService) {
+        this.answerLogicService = answerLogicService;
+        this.boardLogicService = boardLogicService;
+    }
 
     @GetMapping("")
     public String listview(
@@ -27,29 +29,28 @@ public class BoardViewPageController {
             Model model) {
         Board eachBoard = boardLogicService.readBoardview(id);
 
-        modelAdd(eachBoard, model,"Answer");
-        modelAdd(answerLogicService.readUser(eachBoard), model,"eachboard");
-
-        modelAdds(answerLogicService.readAnswer(id),model,"Answers");
-        modelAdds(boardLogicService.bestweekview(getCategoryId(eachBoard)),model,"weekview");
-        modelAdds(boardLogicService.bestmonthview(getCategoryId(eachBoard)),model,"monthview");
-
+        model.addAttribute("Answer", eachBoard);
+        model.addAttribute("eachboard", answerLogicService.readUser(eachBoard));
+        model.addAttribute("Answers", answerLogicService.readAnswer(id));
+        model.addAttribute("weekview", boardLogicService.bestweekview(getCategoryId(eachBoard)));
+        model.addAttribute("monthview", boardLogicService.bestmonthview(getCategoryId(eachBoard)));
 
         return "pages/view";
     }
+
     @PostMapping("/answer")
     public String postAnswer(@Valid Answer answer, BindingResult bindingResult, Model model) {
         String id = String.valueOf(answer.getBoard().getId());
         if (bindingResult.hasErrors()) {
-            modelAdd(answer.getBoard(),model,"eachboard");
-            modelAdd(answer,model,"Answer");
-
-            modelAdds(answerLogicService.readAnswer(id),model,"Answers");
+            model.addAttribute("eachboard", answer.getBoard());
+            model.addAttribute("Answer", answer);
+            model.addAttribute("Answers", answerLogicService.readAnswer(id));
             return "pages/view";
         }
         Answer answer1 = answerLogicService.save(answer);
         return "redirect:/board/view?id=" + answer1.getBoard().getId();
     }
+
     @GetMapping("/answer/delete")
     public String deleteAnswer(
             @RequestParam(value = "id", required = false, defaultValue = "0") String id,
@@ -58,15 +59,8 @@ public class BoardViewPageController {
         return "redirect:/board/view?id=" + boardid;
     }
 
-    private String getCategoryId(Board board){
+    private String getCategoryId(Board board) {
         String categoryId = String.valueOf(board.getCategory().getId());
         return categoryId;
-    }
-
-    private void modelAdd(Object obj, Model model, String str){
-        model.addAttribute(str,obj);
-    }
-    private void modelAdds(List<?> objs, Model model, String str){
-        model.addAttribute(str,objs);
     }
 }
