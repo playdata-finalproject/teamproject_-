d5e45485c975e71b440a6b459a7c75ebacae7975
jeeseungbae <72952484+jeeseungbae@users.noreply.github.com>
2021-07-06 16:08:52 +0900

    ProfileController logic 및 네이밍 변경 (#122)
    * refector : ProfileController logic 변경 메서드 추가
    
    close #121

--- app/src/main/java/com/finalproject/shelter/presentation/controller/view/profile/ProfileController.java
+++ app/src/main/java/com/finalproject/shelter/presentation/controller/view/profile/ProfileController.java
@@ -17,14 +17,18 @@ public class ProfileController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/{username}")
-    public String view(@PathVariable String username, Model model){
+    public String view(@PathVariable String username, Model model) {
         Account byUsername = accountRepository.findByUsername(username);
-        if (byUsername == null) {
-            throw new IllegalArgumentException(byUsername + " 에 해당하는 사용자가 없습니다.");
-        }
+        isByUsernameEqualsNull(byUsername);
         model.addAttribute(byUsername);
-        model.addAttribute("isOwner",1);
+        model.addAttribute("isOwner", 1);
         return "account/profile";
+    }
+
+    private void isByUsernameEqualsNull(Account byUsername) {
+        if (byUsername == null) {
+            throw new IllegalArgumentException("Username" + " 에 해당하는 사용자가 없습니다.");
+        }
     }
 
 }
